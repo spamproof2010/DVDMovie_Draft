cab10eb5624761f62caad01a03fd6a0168c0c752
unknown <reynald.consultant@gmail.com>
2019-01-23 12:15:08 -0800

    3.1 Creating the Display Component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ModelModule } from "./models/model.module";
+import { MovieTableComponent } from "./structure/movieTable.component"
+import { CategoryFilterComponent } from "./structure/categoryFilter.component"
 
 @NgModule({
   declarations: [
+    MovieTableComponent, 
+    CategoryFilterComponent,
     AppComponent
   ],
   imports: [

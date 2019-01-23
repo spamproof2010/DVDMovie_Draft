17c551211b559be2fc9f49ce0adebda1c138e10e
unknown <reynald.consultant@gmail.com>
2019-01-23 14:49:42 -0800

    Navigating Back From Details

--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -4,7 +4,8 @@ import { MovieDetailComponent } from "./structure/movieDetail.component";
 
 const routes: Routes = [
  { path: "table", component: MovieTableComponent },
+ { path: "detail/:id", component: MovieDetailComponent },
  { path: "detail", component: MovieDetailComponent },
  { path: "", component: MovieTableComponent }]
- 
+
 export const RoutingConfig = RouterModule.forRoot(routes);

6312a2bc5e921bc6ef34468c6d7df7e33dd482ea
unknown <reynald.consultant@gmail.com>
2019-01-25 14:11:10 -0800

    5.1 Creating the Shopping Cart

--- ClientApp/src/app/store/movieList.component.ts
+++ ClientApp/src/app/store/movieList.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 import { Repository } from "../models/repository";
 import { Movie } from "../models/movie.model";
+import { Cart } from "../models/cart.model";
 
 @Component({
     selector: "store-movie-list",
     templateUrl: "movieList.component.html"
 })
 export class MovieListComponent {
-    constructor(private repo: Repository) { }
+    constructor(private repo: Repository,private cart: Cart) { }
     get movies(): Movie[] {
         if (this.repo.movies != null && this.repo.movies.length > 0) {
             let pageIndex = (this.repo.pagination.currentPage - 1)
@@ -16,5 +17,8 @@ export class MovieListComponent {
                 pageIndex + this.repo.pagination.moviesPerPage);
         }
     }
-    
+    addToCart(movie: Movie) {
+        this.cart.addMovie(movie);
+    }
+
 }

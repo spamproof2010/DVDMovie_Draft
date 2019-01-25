486d801aeeb378ced6eb70a01a7df2c7c615a8b5
unknown <reynald.consultant@gmail.com>
2019-01-25 15:43:20 -0800

    5.5 Checkout Process Pt 1

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -3,9 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core'; 
 import { Filter, Pagination } from "./configClasses.repository";
 import { Studio } from "./studio.model";
+import { Order } from "./order.model";
 
 const studiosUrl = "/api/studios";
 const moviesUrl = "/api/movies";
+const ordersUrl = "/api/orders";
 @Injectable()
 export class Repository {
 	
@@ -117,11 +119,32 @@ export class Repository {
 		getSessionData(dataType: string): any {
 			return this.http.get("/api/session/" + dataType);
 		}
+		getOrders() {
+			this.http.get<Order[]>(ordersUrl)
+				.subscribe(data => this.orders = data);
+		}
+		createOrder(order: Order) {
+			this.http.post<any>(ordersUrl, {
+				name: order.name,
+				address: order.address,
+				payment: order.payment,
+				movies: order.movies
+			}).subscribe(data => {
+				order.orderConfirmation = data
+				order.cart.clear();
+				order.clear();
+			});
+		}
+		shipOrder(order: Order) {
+			this.http.post(ordersUrl + "/" + order.orderId,null)
+				.subscribe(r => this.getOrders())
+		}
 	
 	movie: Movie;
 	movies: Movie[];
 	studios: Studio[] = [];
 	categories: string[] = [];
+	orders: Order[] = [];
 	
 	get filter(): Filter {
 		return this.filterObject;
--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -9,6 +9,8 @@ namespace DVDMovie.Models
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Studio> Studios { get; set; }
         public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Movie>().HasMany<Rating>(m => m.Ratings)

5b036c180866201bcc35eeb5503ea78bf0b2e4b3
unknown <reynald.consultant@gmail.com>
2019-01-25 16:23:02 -0800

    5.8 Extending the Session Data Web Service

--- ClientApp/src/app/models/order.model.ts
+++ ClientApp/src/app/models/order.model.ts
@@ -1,10 +1,37 @@
 import { Injectable } from "@angular/core";
 import { Cart } from "./cart.model";
 import { Repository } from "./repository";
+import { Router, NavigationStart } from "@angular/router";
+import "rxjs/add/operator/filter";
 
 @Injectable()
 export class Order {
-    constructor(private repo: Repository, public cart: Cart) { }
+    constructor(private repo: Repository, public cart: Cart,router: Router) {
+        router.events
+        .filter(event => event instanceof NavigationStart)
+        .subscribe(event => {
+            if (router.url.startsWith("/checkout")
+                && this.name != null && this.address != null) {
+                repo.storeSessionData("checkout", {
+                    name: this.name,
+                    address: this.address,
+                    cardNumber: this.payment.cardNumber,
+                    cardExpiry: this.payment.cardExpiry,
+                    cardSecurityCode: this.payment.cardSecurityCode
+                });
+            }
+        });
+        repo.getSessionData("checkout").subscribe(data => {
+            if (data != null) {
+                this.name = data.name;
+                this.address = data.address;
+                this.payment.cardNumber = data.cardNumber;
+                this.payment.cardExpiry = data.cardExpiry;
+                this.payment.cardSecurityCode = data.cardSecurityCode;
+            }
+        })
+    }
+
     orderId: number;
     name: string;
     address: string;
--- Controllers/SessionValuesController.cs
+++ Controllers/SessionValuesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using DVDMovie.Models;
+using DVDMovie.Models.BindingTargets;
 
 namespace DVDMovie.Controllers
 {
@@ -18,6 +19,17 @@ namespace DVDMovie.Controllers
         {
             var jsonData = JsonConvert.SerializeObject(movies);
             HttpContext.Session.SetString("cart", jsonData);
+        }
+                [HttpGet("checkout")]
+        public IActionResult GetCheckout()
+        {
+            return Ok(HttpContext.Session.GetString("checkout"));
+        }
+        [HttpPost("checkout")]
+        public void StoreCheckout([FromBody] CheckoutState data)
+        {
+            HttpContext.Session.SetString("checkout",
+            JsonConvert.SerializeObject(data));
         }
     }
 }

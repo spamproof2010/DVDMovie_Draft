e532b98a06af16740465a11a1276ec4bd5de51a0
unknown <reynald.consultant@gmail.com>
2019-01-27 16:53:31 -0800

    6.2 Administering Movies

--- ClientApp/src/app/admin/admin.module.ts
+++ ClientApp/src/app/admin/admin.module.ts
@@ -6,9 +6,11 @@ import { AdminComponent } from "./admin.component";
 import { OverviewComponent } from "./overview.component";
 import { MovieAdminComponent } from "./movieAdmin.component";
 import { OrderAdminComponent } from "./orderAdmin.component";
+import { MovieEditorComponent } from "./movieEditor.component";
+
 @NgModule({
     imports: [BrowserModule, RouterModule, FormsModule],
     declarations: [AdminComponent, OverviewComponent,
-        MovieAdminComponent, OrderAdminComponent]
+        MovieAdminComponent, OrderAdminComponent,MovieEditorComponent]
 })
 export class AdminModule { }
